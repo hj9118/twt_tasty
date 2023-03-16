259f00307399f3003692b11f325564f9884be8a1
jiho9118 <mexocoxon@gmail.com>
2023-03-16 20:03:02 +0900

    form 형식 삭제

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import { useState } from 'react';
 function App() {
   const [value, setValue] = useState('');
   const thread = () => {
-    window.open(`https://twitter.com/search?q=min_faves%3A50%20min_retweets%3A100%20${value}%20타래`);
+    window.open(
+      `https://twitter.com/search?q=min_faves%3A50%20min_retweets%3A100%20${value}%20타래`
+    );
   };
   const keyword = () => {
-    window.open(`https://twitter.com/search?q=min_faves%3A50%20min_retweets%3A100%20${value}%20맛집`)
-  }
+    window.open(
+      `https://twitter.com/search?q=min_faves%3A50%20min_retweets%3A100%20${value}%20맛집`
+    );
+  };
   const handleChange = (e) => {
     setValue(e.target.value);
   };
@@ -17,16 +21,14 @@ function App() {
     <Wrapper>
       <BsTwitter fontSize='2rem' />
       <h1>맛집 검색</h1>
-      <form>
-        <Input
-          type='text'
-          onChange={handleChange}
-          placeholder='지역명 혹은 키워드로 검색'
-        />
-        <br />
-        <Button onClick={keyword}>키워드 검색</Button>
-        <Button onClick={thread}>타래 검색</Button>
-      </form>
+      <Input
+        type='text'
+        onChange={handleChange}
+        placeholder='지역명 혹은 키워드로 검색'
+      />
+      <br />
+      <Button onClick={keyword}>키워드 검색</Button>
+      <Button onClick={thread}>타래 검색</Button>
     </Wrapper>
   );
 }
